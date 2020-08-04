8203e1faf977a8488de6d470c1b0bca70ee314b2
Cihan Eran <erancihan@gmail.com>
2020-08-04 20:36:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import mimetypes
 from datetime import datetime
+import mimetypes
 from threading import Thread
 
 import torch
 from flask import Flask, render_template, send_from_directory, request, jsonify
+from torchvision.transforms import ToPILImage
 
-from ml_model import Model
+from ml_model import Model, process_request, unnormalize_batch, MEAN, STD
 
 model: torch.nn.Module
 is_model_loaded = False
 
-SAVE_DEBUG_IMGS = True
+SAVE_DEBUG_IMAGES = True
 
 
 def get_model():
@@ -46,7 +47,22 @@ def img_upload():
     if not is_model_loaded:
         return jsonify({})
 
-    _t = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    image = request.json['image']
+    mask = request.json['mask']
+    ext = request.json['ext']
+
+    img, msk = process_request(image, mask, ext, save_debug_images=SAVE_DEBUG_IMAGES)
+
+    out = model.forward(img, msk)
+    out = msk * unnormalize_batch(img, MEAN, STD) + (1.0 - msk) * out
+    out = ToPILImage()(out.squeeze().cpu())
+
+    if SAVE_DEBUG_IMAGES:
+        out.save("_out-{}.png".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")), ext)
+
+    # TODO DEBUG & VALIDATE prediction
+    # TODO respond/send byte to client
+    # out.tobytes()
 
     return jsonify({})
 
